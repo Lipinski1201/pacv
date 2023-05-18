cf5a0768521f3072911ce63b470457f2db4e07a6
Lipinski1201 <gustavolipinski12@gmail.com>
2023-05-17 21:37:54 -0300

    criacao das rotas

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import RootLayout from "./layout"
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-cyan-900 to-gray-800">
+      <h1 className='text-5xl font-bold'>Login</h1>
+      <a href=""></a>
+      <button className="p-4 rounded-3xl bg-red-700 text-3xl font-bold   transition-all">Logar</button>
+      <div className="">
+        <forms>
+          
+        </forms>
+
+      </div>
+    </main>
+  )
+}
