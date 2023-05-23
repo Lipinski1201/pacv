f060f2fd00d32f6117eb25180496d5941f3bc763
Lipinski1201 <gustavolipinski12@gmail.com>
2023-05-23 17:51:49 -0300

    raajuste cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Link from 'next/link'
 
 
 const Form = () => {
@@ -6,18 +7,50 @@ const Form = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+
+    if (name === '' || email === '' || senha === '' || confirmaSenha === '') {
+      setError('Por favor, preencha todos os campos.');
+      setSuccess(false);
+      return;
+    }
+
     // Lógica de envio do formulário
     console.log('Nome:', name);
     console.log('Email:', email);
     console.log('Senha:', senha);
-    
+
+    // Limpar os campos e o estado de erro após o envio do formulário
+    setSuccess(true);
+    setName('');
+    setEmail('');
+    setSenha('');
+    setConfirmaSenha('');
+    setError('');
+
+  };
+  const handleCloseSuccess = () => {
+    setSuccess(false);
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-cyan-900 to-gray-800">
+    <div className="flex items-center flex-col justify-center h-screen bg-gradient-to-b from-cyan-900 to-gray-800">
+      {success && (
+        <div className="rounded-md bg-green-50 p-4 text-sm text-green-500 mt-10 mb-10">
+          <b>Sucesso:</b> Usuário cadastrado com sucesso!
+          <button
+            className="ml-2 text-blue-500 hover:text-blue-700 font-bold"
+            onClick={handleCloseSuccess}
+          >
+            Fechar
+          </button>
+        </div>
+      )}
       <div className="max-w-md w-full px-6 py-8 bg-gradient-to-b from-cyan-900 to-gray-800 border-2 border-blue-500">
         <h2 className="text-2xl font-bold mb-4">Formulário de Cadastro</h2>
         <form onSubmit={handleSubmit}>
@@ -77,17 +110,30 @@ const Form = () => {
               required
             />
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center space-x-3">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Enviar
             </button>
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              <Link href="/">Voltar</Link>
+            </button>
           </div>
+          {error && (
+            <div className="rounded-md bg-red-50 p-4 text-sm text-red-500 mt-4">
+              <b>Erro:</b> {error}
+            </div>
+          )}
+
         </form>
       </div>
     </div>
+
   );
 };
 
