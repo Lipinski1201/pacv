266c651e17f66e0c37e57973490536ae99f5e762
Luis Felipe <61890740+LuisFelipegp@users.noreply.github.com>
2023-05-23 09:17:51 -0300

    alteração de tela de login 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,37 @@
 import RootLayout from "./layout"
+import imagens from "./imagens/logo-sem-fundo.png"
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-cyan-900 to-gray-800">
-      <h1 className='text-5xl font-bold'>Login</h1>
-      <a href=""></a>
-      <button className="p-4 rounded-3xl bg-red-700 text-3xl font-bold   transition-all">Logar</button>
-      <div className="">
-        <forms>
+      <h1 className='text-5xl font-bold'>Entre em sua conta</h1>
           
-        </forms>
+      <a href=""></a>
+      
+      <div className=" text-white">
+        <form>
+          <div className="font-bold w-20">
+            <label htmlFor="email">E-mail</label>
+              <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Digite seu email"
+              />
+          </div>
+          <div className="font-bold w-20 ">
+            <label htmlFor="password">Senha</label>
+              <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="*********"
+            />
+          </div>
+          </form>
+      </div>
+      <button className="p-4 rounded-3xl bg-blue-900 text-3xl font-bold   transition-all">Entrar</button>
+      <div className="">
 
       </div>
     </main>
